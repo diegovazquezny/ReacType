005a203b5419a56a66649fd804237527975e2628
Diego Vazquez <diegovazquezny@gmail.com>
2020-11-11 15:47:09 -0500

    refactored Tree code to TS

--- app/src/tree/TreeChart.tsx
+++ app/src/tree/TreeChart.tsx
@@ -2,45 +2,47 @@ import React, { useRef, useEffect, useContext } from "react";
 import { select, hierarchy, tree, linkHorizontal } from "d3";
 import useResizeObserver from "./useResizeObserver";
 import StateContext from '../context/context';
+import { Component } from '../interfaces/Interfaces';
 
 function usePrevious(value) {
-  const ref = useRef();
+  const ref = useRef<Array<Component>>(null);
   useEffect(() => {
     ref.current = value;
   });
   return ref.current;
 }
 
-function TreeChart({ data }) {
+const TreeChart = ({ data }): JSX.Element => {
   const [state, dispatch] = useContext(StateContext);
-  const canvasId = state.canvasFocus.componentId;
+  const canvasId: number = state.canvasFocus.componentId;
 
-  const svgRef = useRef();
-  const wrapperRef = useRef();
+  const svgRef = useRef<SVGSVGElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  
+  const xPosition: number = 50;
+  const textAndBorderColor: string = 'rgb(51, 235, 145)';
 
-  const xPosition = 50;
-  const textAndBorderColor = 'rgb(51, 235, 145)';
-
-  const dimensions = useResizeObserver(wrapperRef);
+  const dimensions: DOMRectReadOnly = useResizeObserver(wrapperRef);
 
   // we save data to see if it changed
-  const previouslyRenderedData = usePrevious(data);
+  const previouslyRenderedData: Array<Component> = usePrevious(data);
 
   // will be called initially and on every data change
   useEffect(() => {
     const svg = select(svgRef.current);
+    
     // use dimensions from useResizeObserver,
     // but use getBoundingClientRect on initial render
     // (dimensions are null for the first render)
     const { width, height } =
-    dimensions || wrapperRef.current.getBoundingClientRect();
+      dimensions || wrapperRef.current.getBoundingClientRect();
     // transform hierarchical data
     const root = hierarchy(data[canvasId - 1]);
-    const treeLayout = tree().size([height, width - 125]);
+    const treeLayout = tree<Component>().size([height, width - 125]);
 
     // Returns a new link generator with horizontal display.
     // To visualize links in a tree diagram rooted on the left edge of the display
-    const linkGenerator = linkHorizontal()
+    const linkGenerator: void = linkHorizontal()
       .x(link => link.y)
       .y(link => link.x);
 
@@ -66,8 +68,10 @@ function TreeChart({ data }) {
       .style('fill', 'white')
       .attr('transform', `translate(${xPosition}, 0)`);
 
+      console.log(svg);
+
     // link - lines that connect the nodes
-    const enteringAndUpdatingLinks = svg
+    svg
       .selectAll('.link')
       .data(root.links())
       .join('path')
@@ -78,13 +82,14 @@ function TreeChart({ data }) {
       .attr('opacity', 1)
       .attr('transform', `translate(${xPosition}, 0)`);
 
-    if (data !== previouslyRenderedData) {
-      enteringAndUpdatingLinks
-        .attr('stroke-dashoffset', function() {
-          return this.getTotalLength();
-        })
-        .attr('stroke-dashoffset', 0);
-    }
+    // if (data !== previouslyRenderedData) {
+    //   enteringAndUpdatingLinks
+    //     .attr('stroke-dashoffset', function() {
+    //       console.log(this.getTotalLength());
+    //       return this.getTotalLength();
+    //     })
+    //     .attr('stroke-dashoffset', 100);
+    // }
 
     // label - the names of each html element (node)
     svg
@@ -117,12 +122,11 @@ function TreeChart({ data }) {
     display: 'flex',
     justifyContent: 'center'
   };
+
   return (
-    // <div theme={theme} style={{ backgroundColor: 'black' }}>
     <div ref={wrapperRef} style={wrapperStyles}>
       <svg ref={svgRef} style={treeStyles}></svg>
     </div>
-    // </div>
   );
 }
 
--- app/src/tree/useResizeObserver.ts
+++ app/src/tree/useResizeObserver.ts
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import ResizeObserver from "resize-observer-polyfill";
 
-const useResizeObserver = ref => {
+/*
+this function returns the dimensions of the div containing
+the tree. The tree will resize depending on these dimensions.
+*/
+
+const useResizeObserver = (ref) => {
   const [dimensions, setDimensions] = useState(null);
   useEffect(() => {
     // the element being observed (div with green border)
-    const observeTarget = ref.current;
+    const observeTarget: HTMLDivElement = ref.current;
     const resizeObserver = new ResizeObserver(entries => {
         entries.forEach(entry => {
-          // contentRect is an object containing the dimensions of the observed element
+        // contentRect is an object containing the dimensions of the observed element
         setDimensions(entry.contentRect);
       });
     });
@@ -17,7 +22,7 @@ const useResizeObserver = ref => {
       resizeObserver.unobserve(observeTarget);
     };
   }, [ref]);
-  return dimensions; // { height, width }
+  return dimensions;
 };
 
 export default useResizeObserver;
