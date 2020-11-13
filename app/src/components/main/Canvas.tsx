63b2002387dc3c0a5cd1f2f80de7f4db5b73c910
Diego Vazquez <diegovazquezny@gmail.com>
2020-11-13 17:25:06 -0500

    merging final pr

--- app/src/components/main/Canvas.tsx
+++ app/src/components/main/Canvas.tsx
@@ -5,38 +5,31 @@ import StateContext from '../../context/context';
 import { Component, DragItem } from '../../interfaces/Interfaces';
 import { combineStyles } from '../../helperFunctions/combineStyles';
 import renderChildren from '../../helperFunctions/renderChildren';
-
 const findNestedChild = (curr, components) => {
   components.forEach((comp, i) => {
     comp.children.forEach(child => {
-      if (child.name === curr.name) console.log('childname', child.name);
+      if (child.name === curr.name) {};
     });
     if (comp.children.length !== 0) findNestedChild(curr, comp.children);
   });
 };
-
 function Canvas() {
   const [state, dispatch] = useContext(StateContext);
-  console.log('components', state.components);
   // find the current component to render on the canvas
   const currentComponent: Component = state.components.find(
     (elem: Component) => elem.id === state.canvasFocus.componentId
   );
-
   findNestedChild(currentComponent, state.components);
-
   // changes focus of the canvas to a new component / child
   const changeFocus = (componentId: number, childId: number | null) => {
     dispatch({ type: 'CHANGE FOCUS', payload: { componentId, childId } });
   };
-
   // onClickHandler is responsible for changing the focused component and child component
   function onClickHandler(event) {
     event.stopPropagation();
     // note: a null value for the child id means that we are focusing on the top-level component rather than any child
     changeFocus(state.canvasFocus.componentId, null);
   }
-
   // This hook will allow the user to drag items from the left panel on to the canvas
   const [{ isOver }, drop] = useDrop({
     accept: ItemTypes.INSTANCE,
@@ -45,7 +38,6 @@ function Canvas() {
       if (didDrop) {
         return;
       }
-
       // if item dropped is going to be a new instance (i.e. it came from the left panel), then create a new child component
       if (item.newInstance) {
         dispatch({
@@ -72,7 +64,6 @@ function Canvas() {
       isOver: !!monitor.isOver()
     })
   });
-
   const defaultCanvasStyle = {
     width: '100%',
     minHeight: '100%',
@@ -80,11 +71,9 @@ function Canvas() {
     border: '3px solid #01d46d',
     borderStyle: isOver ? 'dotted' : 'solid'
   };
-
   // Combine the default styles of the canvas with the custom styles set by the user for that component
   // The render children function renders all direct children of a given component
   // Direct children are draggable/clickable
-
   const canvasStyle = combineStyles(defaultCanvasStyle, currentComponent.style);
   return (
     <div ref={drop} style={canvasStyle} onClick={onClickHandler}>
@@ -92,5 +81,4 @@ function Canvas() {
     </div>
   );
 }
-
 export default Canvas;
